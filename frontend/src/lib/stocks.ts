f8a3e06cec3ccc439243e320d7d35c4f82b832bf
imlocle <loc.tan.le@gmail.com>
2025-10-22 21:04:58 -0700

    global set ups, StockCard, LoadingCircle, lib, interfaces

---
+++ frontend/src/lib/stocks.ts
@@ -0,0 +1,24 @@
+export interface StockInfo {
+  symbol: string;
+  domain: string;
+}
+
+
+
+export const STOCKS: StockInfo[] = [
+  { symbol: "AAPL", domain: "apple.com" },
+  { symbol: "ADBE", domain: "adobe.com" },
+  { symbol: "AMZN", domain: "amazon.com" },
+  { symbol: "CRM", domain: "salesforce.com" },
+  { symbol: "CSCO", domain: "cisco.com" },
+  { symbol: "FB", domain: "meta.com" },
+  { symbol: "GOOGL", domain: "abc.xyz" },
+  { symbol: "INTC", domain: "intel.com" },
+  { symbol: "MSFT", domain: "microsoft.com" },
+  { symbol: "NFLX", domain: "netflix.com" },
+  { symbol: "NVDA", domain: "nvidia.com" },
+  { symbol: "ORCL", domain: "oracle.com" },
+  { symbol: "PYPL", domain: "paypal.com" },
+  { symbol: "TSLA", domain: "tesla.com" },
+  { symbol: "UBER", domain: "uber.com" },
+];
